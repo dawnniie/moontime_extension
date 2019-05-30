1f74d0bd1ab41593c50510f687c12a0731b7f1ea
LachlanWalls <lachlan.walls1@gmail.com>
2019-05-30 20:48:22 +1000

    Added timekeeping

---
+++ script.js
@@ -0,0 +1,12 @@
+window.setTimeout(() => {
+    document.body.removeAttribute("preload")
+}, 10)
+
+window.addEventListener("moontime:updated", function(e) {
+    document.querySelector("#section_1 .time .time").innerHTML = moon.formatMoonString("%MS:%MM:%M")
+    document.querySelector("#section_1 .time .date").innerHTML = moon.formatMoonString("%MdT %MeMT, %MAT, %MC")
+})
+
+window.addEventListener("moontime:offset_updated", function(e) {
+    console.log("Offset: " + e.detail)
+})
